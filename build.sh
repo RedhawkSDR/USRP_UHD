74dde0b4e9f51c76fdd2ea2985a94efab54a65f4
Kipp Bowen <jbowen@lgsinnovations.com>
2017-11-30 14:46:27 -0500

    Fix build.sh version number to be consistent with spec/etc.

--- build.sh
+++ build.sh
@@ -24,9 +24,9 @@ if [ "$1" = "rpm" ]; then
     if [ -e rh.USRP_UHD.spec ]; then
         mydir=`dirname $0`
         tmpdir=`mktemp -d`
-        cp -r ${mydir} ${tmpdir}/rh.USRP_UHD-5.0.1
-        tar czf ${tmpdir}/rh.USRP_UHD-5.0.1.tar.gz --exclude=".svn" --exclude=".git" -C ${tmpdir} rh.USRP_UHD-5.0.1
-        rpmbuild -ta ${tmpdir}/rh.USRP_UHD-5.0.1.tar.gz
+        cp -r ${mydir} ${tmpdir}/rh.USRP_UHD-6.0.0
+        tar czf ${tmpdir}/rh.USRP_UHD-6.0.0.tar.gz --exclude=".svn" --exclude=".git" -C ${tmpdir} rh.USRP_UHD-6.0.0
+        rpmbuild -ta ${tmpdir}/rh.USRP_UHD-6.0.0.tar.gz
         rm -rf $tmpdir
     else
         echo "Missing RPM spec file in" `pwd`
